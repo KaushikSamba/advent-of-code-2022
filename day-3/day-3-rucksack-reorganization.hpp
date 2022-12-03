55d10f4cf8c54b972aae02fc5a32370795abbf30
Kaushik Sambamurthy <kaushik.sambamurthy@alertinnovation.com>
2022-12-03 17:40:28 +0000

    [DAY-3] Parse input file

--- day-3/day-3-rucksack-reorganization.hpp
+++ day-3/day-3-rucksack-reorganization.hpp
@@ -1,7 +1,12 @@
 #ifndef DAY_3_RUCKSACK_REORGANIZATION_HPP
 #define DAY_3_RUCKSACK_REORGANIZATION_HPP
 
+#include <string>
+#include <vector>
+
 namespace rucksack
-{}  // namespace rucksack
+{
+std::vector<std::string> parseInputs(std::istream& stream);
+}  // namespace rucksack
 
 #endif  // DAY_3_RUCKSACK_REORGANIZATION_HPP
--- test/day-3/test.cpp
+++ test/day-3/test.cpp
@@ -1,3 +1,4 @@
+#include "day-3-rucksack-reorganization.hpp"
 #include "utils/filehandler.hpp"
 #include <gtest/gtest.h>
 
@@ -5,4 +6,20 @@ TEST(Day3Tests, AbleToReadInputFile)
 {
     utils::FileHandler inputFile {CURRENT_SOURCE_DIR "/test-input.txt"};
     EXPECT_TRUE(inputFile);
+}
+
+TEST(Day3Tests, ParseInputFile)
+{
+    utils::FileHandler inputFile {CURRENT_SOURCE_DIR "/test-input.txt"};
+
+    std::vector<std::string> expectedResult {
+        "vJrwpWtwJgWrhcsFMMfFFhFp",
+        "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+        "PmmdzqPrVvPwwTWBwg",
+        "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+        "ttgJtRGJQctTZtZT",
+        "CrZsJsPPZsGzwwsLwLmpwMDw",
+    };
+
+    EXPECT_EQ(rucksack::parseInputs(inputFile), expectedResult);
 }

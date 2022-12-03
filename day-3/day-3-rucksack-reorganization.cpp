08dd6b33efffd4001d88fd2aeda2f78839dd28a0
Kaushik Sambamurthy <kaushik.sambamurthy@alertinnovation.com>
2022-12-03 17:40:28 +0000

    [DAY-3] Parse input file

--- day-3/day-3-rucksack-reorganization.cpp
+++ day-3/day-3-rucksack-reorganization.cpp
@@ -1,4 +1,12 @@
 #include "day-3-rucksack-reorganization.hpp"
+#include <functional>
+#include "utils/input_parser.hpp"
 
 namespace rucksack
-{}  // namespace rucksack
+{
+std::vector<std::string> parseInputs(std::istream& stream)
+{
+    return utils::parsing::parseSingleLineInputsToType<std::string>(stream, [](const auto& str) { return str; });
+}
+
+}  // namespace rucksack

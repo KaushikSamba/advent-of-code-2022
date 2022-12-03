85d4c8e6d70e65fe39ddc7821ff3a18cfa0faaf8
Kaushik Sambamurthy <kaushik.sambamurthy@alertinnovation.com>
2022-12-03 17:52:25 +0000

    [DAY-3] Split string into N equal parts

--- day-3/day-3-rucksack-reorganization.cpp
+++ day-3/day-3-rucksack-reorganization.cpp
@@ -9,4 +9,16 @@ std::vector<std::string> parseInputs(std::istream& stream)
     return utils::parsing::parseSingleLineInputsToType<std::string>(stream, [](const auto& str) { return str; });
 }
 
+std::vector<std::string> splitCompartments(std::string const& contents, std::size_t numParts)
+{
+    auto                     size      = contents.size();
+    std::size_t              splitSize = size / numParts;
+    std::vector<std::string> splits;
+
+    for(std::size_t i = 0; i < size; i += splitSize)
+    {
+        splits.push_back(contents.substr(i, splitSize));
+    }
+    return splits;
+}
 }  // namespace rucksack
--- day-3/day-3-rucksack-reorganization.hpp
+++ day-3/day-3-rucksack-reorganization.hpp
@@ -7,6 +7,8 @@
 namespace rucksack
 {
 std::vector<std::string> parseInputs(std::istream& stream);
+
+std::vector<std::string> splitCompartments(std::string const& contents, std::size_t numParts);
 }  // namespace rucksack
 
 #endif  // DAY_3_RUCKSACK_REORGANIZATION_HPP
